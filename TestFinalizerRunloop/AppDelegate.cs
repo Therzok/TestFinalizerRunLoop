e1b923f91a559a615d54a730e4c6264f8b0ed95d
Marius Ungureanu <maungu@microsoft.com>
2021-11-03 20:14:16 +0200

    More stuff

--- TestFinalizerRunloop/AppDelegate.cs
+++ TestFinalizerRunloop/AppDelegate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using AppKit;
+using CoreFoundation;
 using Foundation;
 
 namespace TestFinalizerRunloop
@@ -9,7 +11,6 @@ namespace TestFinalizerRunloop
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
-        WeakReference<NSWindow> _wnd = new WeakReference<NSWindow>(null);
         public AppDelegate()
         {
         }
@@ -19,10 +20,17 @@ namespace TestFinalizerRunloop
 
         public override void DidFinishLaunching(NSNotification notification)
         {
+            NSWindowController controller;
+
+            controller = NSStoryboard.MainStoryboard.InstantiateControllerWithIdentifier("ReleaseWhenClosed") as NSWindowController;
+            controller.ShowWindow(this);
+
+            controller = NSStoryboard.MainStoryboard.InstantiateControllerWithIdentifier("NotReleaseWhenClosed") as NSWindowController;
+            controller.ShowWindow(this);
+
             NSNotificationCenter.DefaultCenter.AddObserver(NSWindow.WillCloseNotification, notif =>
             {
                 var wnd = (NSWindow)notif.Object;
-                _wnd.SetTarget(wnd);
                 Console.WriteLine("Closing {0} - ReleaseWhenClosed {1}", wnd, wnd.ReleasedWhenClosed);
 
                 //objc_msgSend(wnd.Handle, ObjCRuntime.Selector.GetHandle("setContentView:"), IntPtr.Zero);
@@ -33,32 +41,117 @@ namespace TestFinalizerRunloop
             {
                 Console.WriteLine("Window changed: {0}", notif.Object);
             });
-
-            _ = WaitForGC();
-
-            static async Task WaitForGC()
-            {
-                while (true)
-                {
-                    await Task.Delay(1000);
-
-                    Console.WriteLine("[GC] Running GC...");
-
-                    using var pool = new NSAutoreleasePool();
-
-                    _ = new int[100000];
-
-                    System.GC.Collect();
-                    System.GC.WaitForPendingFinalizers();
-                }
-            }
         }
 
         public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
         {
             Console.WriteLine("ShouldTerminate: {0}", sender);
 
+            _ = Task.Run(() => WaitForGC());
+
             return false;
+
+            static async Task WaitForGC()
+            {
+                // If this is run on the UI thread, hello crash.
+                while (true)
+                {
+                    await Task.Delay(1000);
+
+                    Console.WriteLine("[GC] Running GC...");
+
+                    _ = new int[10_000_000];
+
+                    System.GC.Collect();
+
+                    // WaitForPendingFinalizers crashes!
+                    //System.GC.WaitForPendingFinalizers();
+                }
+            }
+        }
+
+        void StressTestGC()
+        {
+            Console.WriteLine("Creating views");
+            Task.Run(() =>
+            {
+
+                for (int n = 0; n < 10000; n++)
+                {
+                    new FinalizerAppKit
+                    {
+                        Child = new FinalizerAppKit
+                        {
+                            Child = new FinalizerAppKit(),
+                        },
+                    };
+                }
+            });
+            Task.Run(() =>
+            {
+                for (int n = 0; n < 10000; n++)
+                {
+                    new FinalizerDispatch
+                    {
+                        Child = new FinalizerDispatch
+                        {
+                            Child = new FinalizerDispatch(),
+                        },
+                    };
+                }
+            });
+            Console.WriteLine("Done");
+        }
+
+        public class FinalizerAppKit : NSObject
+        {
+            static int finalizedCount = 0;
+            static int disposedCount = 0;
+            static readonly NSObject target = new NSObject();
+
+            public FinalizerAppKit Child;
+
+            bool disposed;
+            ~FinalizerAppKit()
+            {
+                if (Interlocked.Increment(ref finalizedCount) % 1000 == 0)
+                    Console.WriteLine("AppKit: {0} {1}", FinalizerAppKit.finalizedCount, FinalizerAppKit.disposedCount);
+
+                target.BeginInvokeOnMainThread(() => Dispose());
+            }
+
+            void Dispose()
+            {
+                disposed = true;
+                if (Interlocked.Increment(ref disposedCount) % 1000 == 0)
+                    Console.WriteLine("AppKit: {0} {1}", FinalizerAppKit.finalizedCount, FinalizerAppKit.disposedCount);
+            }
+        }
+
+        public class FinalizerDispatch : NSObject
+        {
+            static int finalizedCount = 0;
+            static int disposedCount = 0;
+            static readonly DispatchQueue queue = DispatchQueue.MainQueue;
+
+            public FinalizerDispatch Child;
+
+            bool disposed;
+
+            ~FinalizerDispatch()
+            {
+                if (Interlocked.Increment(ref finalizedCount) % 1000 == 0)
+                    Console.WriteLine("Dispatch: {0} {1}", FinalizerDispatch.finalizedCount, FinalizerDispatch.disposedCount);
+
+                queue.DispatchAsync(() => Dispose());
+            }
+
+            void Dispose()
+            {
+                disposed = true;
+                if (Interlocked.Increment(ref disposedCount) % 1000 == 0)
+                    Console.WriteLine("Dispatch: {0} {1}", FinalizerDispatch.finalizedCount, FinalizerDispatch.disposedCount);
+            }
         }
     }
 }
---
+++ TestFinalizerRunloop/FinalizableView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using AppKit;
+
+namespace TestFinalizerRunloop
+{
+	public class FinalizableView : NSView
+	{
+        static int i = 0;
+
+        int id = Interlocked.Increment(ref i);
+
+        public override void ViewWillMoveToWindow(NSWindow newWindow)
+        {
+            Console.WriteLine("[{0}] ViewWillMoveToWindow {1} -> {2}", id, Window?.Title ?? "null", newWindow?.Title ?? "null");
+            base.ViewWillMoveToWindow(newWindow);
+        }
+
+        ~FinalizableView()
+        {
+            Console.WriteLine("[{0}] Byeeee", id);
+        }
+    }
+}
+
